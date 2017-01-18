9b70fc59f006c80f3e3987f4ed2ee6f818ac4602
rouz majlessi <rmajlessi@gmail.com>
2017-01-18 10:58:39 -0800

    fix bug in routehandler for update

--- app/routes/index.js
+++ app/routes/index.js
@@ -12,13 +12,15 @@ export default Ember.Route.extend({
     this.transitionTo('index');
   },
 // if each key in the params is NOT defined take contact and set property that matches current key, to the value of the current key. We do this because we assure the properties the user has update wil be changed properly do what the user wants updated/edited. if we dont do this we get an error of undefined.
-  update(rental, params) {
+  update(contact, params) {
     Object.keys(params).forEach(function(key) {
       if(params[key] !==undefined) {
         contact.set(key,params[key]);
       }
     });
-  }
+    contact.save();
+    this.transitionTo('index');
+  },
 
     destroyContact(contact) {
       contact.destroyRecord();
